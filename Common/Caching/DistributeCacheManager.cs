78edc7d8e91d3be4602e14db0750855346497c6d
tungdteu <dinhtientung1984@gmail.com>
2015-01-01 14:11:02 +0700

    Chịu bố nó rồi viết như nào trong tiếng anh nhỉ :))

---
+++ Common/Caching/DistributeCacheManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using SharedCache.WinServiceCommon.Provider.Cache;
+
+namespace Common.Caching
+{
+    /// <summary>
+    ///     Represents a DistributeCacheManager
+    /// </summary>
+    public class DistributeCacheManager : ICacheManager
+    {
+        /// <summary>
+        ///     Gets or sets the value associated with the specified key.
+        /// </summary>
+        /// <typeparam name="T">Type</typeparam>
+        /// <param name="key">The key of the value to get.</param>
+        /// <returns>The value associated with the specified key.</returns>
+        public T Get<T>(string key)
+        {
+            return IndexusDistributionCache.SharedCache.Get<T>(key);
+        }
+
+        /// <summary>
+        ///     Adds the specified key and object to the cache.
+        /// </summary>
+        /// <param name="key">key</param>
+        /// <param name="data">Data</param>
+        /// <param name="cacheTime">Cache time in minus</param>
+        public void Set(string key, object data, int cacheTime)
+        {
+            if (data == null)
+                return;
+
+            IndexusDistributionCache.SharedCache.Add(key, data, DateTime.Now.AddMinutes(cacheTime));
+        }
+
+        /// <summary>
+        ///     Gets a value indicating whether the value associated with the specified key is cached
+        /// </summary>
+        /// <param name="key">key</param>
+        /// <returns>Result</returns>
+        public bool IsSet(string key)
+        {
+            return IndexusDistributionCache.SharedCache.GetAllKeys().Contains(key);
+        }
+
+        /// <summary>
+        ///     Removes the value with the specified key from the cache
+        /// </summary>
+        /// <param name="key">/key</param>
+        public void Remove(string key)
+        {
+            IndexusDistributionCache.SharedCache.Remove(key);
+        }
+
+        /// <summary>
+        ///     Removes items by pattern
+        /// </summary>
+        /// <param name="pattern">pattern</param>
+        public void RemoveByPattern(string pattern)
+        {
+            IndexusDistributionCache.SharedCache.RegexRemove(pattern);
+        }
+
+        /// <summary>
+        ///     Clear all cache data
+        /// </summary>
+        public void Clear()
+        {
+            IndexusDistributionCache.SharedCache.Clear();
+        }
+    }
+}
---
+++ Common/Caching/ICacheManager.cs
@@ -0,0 +1,58 @@
+﻿namespace Common.Caching
+{
+    /// <summary>
+    ///     ICache manager is an interface to indicate all common methods , properties which going to implement by different
+    ///     cache systems for this project.
+    ///     For example , the memory cache system will have different methodology to implement with share cache system; however
+    ///     each system should have an
+    ///     inheritance all wraped  behaviours that assign in this interface, and other class in projects can manipulate cache
+    ///     system with a same way.
+    /// </summary>
+    /// <remarks>
+    ///     http://www.sirentuan.com/2653434/codep1/best-practices-for-cache-and-session-manager-for-an-mvc-application
+    ///     http://www.nopcommerce.com/
+    ///     Thank for all expert from around the world , I show deep gratitude with your contribution for community !
+    /// </remarks>
+    public interface ICacheManager
+    {
+        /// <summary>
+        ///     Gets or sets the value associated with the specified key.
+        /// </summary>
+        /// <typeparam name="T">Type</typeparam>
+        /// <param name="key">The key of the value to get.</param>
+        /// <returns>The value associated with the specified key.</returns>
+        T Get<T>(string key);
+
+        /// <summary>
+        ///     Adds the specified key and object to the cache.
+        /// </summary>
+        /// <param name="key">key</param>
+        /// <param name="data">Data</param>
+        /// <param name="cacheTime">Cache time</param>
+        void Set(string key, object data, int cacheTime);
+
+        /// <summary>
+        ///     Gets a value indicating whether the value associated with the specified key is cached
+        /// </summary>
+        /// <param name="key">key</param>
+        /// <returns>Result</returns>
+        bool IsSet(string key);
+
+        /// <summary>
+        ///     Removes the value with the specified key from the cache
+        /// </summary>
+        /// <param name="key">/key</param>
+        void Remove(string key);
+
+        /// <summary>
+        ///     Removes items by pattern
+        /// </summary>
+        /// <param name="pattern">pattern</param>
+        void RemoveByPattern(string pattern);
+
+        /// <summary>
+        ///     Clear all cache data
+        /// </summary>
+        void Clear();
+    }
+}
