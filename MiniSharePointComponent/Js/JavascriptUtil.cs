9b0ca6ee18d75ec065919f4cb81994440441d33c
tungdteu <dinhtientung1984@gmail.com>
2015-01-11 22:29:42 +0700

    Develop ChartJs control property and method (try to test)

---
+++ MiniSharePointComponent/Js/JavascriptUtil.cs
@@ -0,0 +1,10 @@
+﻿namespace MiniSharePointComponent.Js
+{
+    public static class JavascriptUtil
+    {
+        public static string WrapScriptToString(string data)
+        {
+            return string.Concat("<script type='text/javascript'>", data, "</script>");
+        }
+    }
+}
